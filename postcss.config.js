8001557e596f1dded2df47982da54a9ffc14b7fd
CalebeEmerick <hello@calebeemerick.com>
2024-02-12 14:10:43 -0300

    🫡

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   plugins: [
       require('tailwindcss'),
-      require('autoprefixer'),
       ...(process.env.JEKYLL_ENV == 'production'
           ? [require('cssnano')({ preset: 'default' })]
           : [])
